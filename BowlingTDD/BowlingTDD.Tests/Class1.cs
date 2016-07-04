ff5d1a761f3f3d0cbe2cf30fcfef42c8e7df973e
jacekbajer <Jacek@102>
2016-07-04 12:38:33 +0100

    When_Less_Than_10_Pins_In_Every_Frame RED

--- BowlingTDD/BowlingTDD.Tests/Class1.cs
+++ BowlingTDD/BowlingTDD.Tests/Class1.cs
@@ -1,12 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace BowlingTDD.Tests
 {
     public class Class1
     {
+        [Test]
+        public void When_Less_Than_10_Pins_In_Every_Frame_Result_Is_Sum_Of_All_Rolls()
+        {
+            var game = new Game();
+
+            var rollList = new List<int> { 2,2,
+                                           3,4,
+                                           5,1,
+                                           7,1,
+                                           9,0,
+                                           5,3,
+                                           4,0,
+                                           4,0,
+                                           2,6,
+                                           4,2 };
+
+            rollList.ForEach(x => game.Roll(x));
+
+            Assert.That(game.Score(), Is.EqualTo(rollList.Sum()));
+        }
+    }
+
+    public class Game
+    {
+        public void Roll(int i)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public object Score()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
